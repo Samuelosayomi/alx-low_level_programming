a7acb080a8b458990221862601f3e0191d272bb0
Samuel Osayomi <Samuelosayomi@github.com>
2023-10-08 02:41:26 -0700

    6 size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float e;
+
+	printf("Size of a char: %lu.\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu.\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu.\n", (unsigned long)size(b));
+	printf("Size of a long long int: %lu.\n", (unsigned long)sizeof(C));
+	printf("Size of a float: %lu.\n", (unsigned long)sizeof(e));
+
+	return (0);
+}
