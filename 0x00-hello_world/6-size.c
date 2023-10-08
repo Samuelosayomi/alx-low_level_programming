7d72889224dc03794fda904fce8fd6d02b2aa55a
Samuel Osayomi <Samuelosayomi@github.com>
2023-10-08 02:49:20 -0700

    6 size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 	printf("Size of a char: %lu.\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %lu.\n", (unsigned long)sizeof(a));
 	printf("Size of a long int: %lu.\n", (unsigned long)size(b));
-	printf("Size of a long long int: %lu.\n", (unsigned long)sizeof(C));
+	printf("Size of a long long int: %lu.\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu.\n", (unsigned long)sizeof(e));
 
 	return (0);
