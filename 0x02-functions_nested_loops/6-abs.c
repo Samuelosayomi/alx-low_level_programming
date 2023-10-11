e9167dc9fc8f0c337357693c744226f853ee4368
Samuel Osayomi <Samuelosayomi@github.com>
2023-10-11 10:04:05 -0700

    6 absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - compute the absolute value of an integer
+ * @i: The integer to be checked
+ * Return: The absolute value of an integer
+ */
+
+int _abs(int i)
+{
+	return (i < 0 ? -i : i);
+}
