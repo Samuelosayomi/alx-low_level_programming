562931b64bd15345519c8d169b919e181d6c2eef
Samuel Osayomi <Samuelosayomi@github.com>
2023-10-11 14:08:50 -0700

    update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,13 +8,10 @@ void jack_bauer(void)
 {
 	int h, m;
 
-	h = 0;
-
-	while (h <= 23)
+	for (h = 0; h <= 23; h++)
 	{
-		m = 0;
 
-		while (m <= 59)
+		for (m = 0; m <= 59; m++)
 		{
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
@@ -22,8 +19,7 @@ void jack_bauer(void)
 			_putchar((m / 10) + '0');
 			_putchar((m % 10) + '0');
 			_putchar('\n');
-			m++;
 		}
-		h++;
+
 	}
 }
