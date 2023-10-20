492d098033aa09b89500f9979061c44862a88b2f
Samuel Osayomi <Samuelosayomi@github.com>
2023-10-20 11:35:55 -0700

    11 print combination of three digit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Program that print all combination of three digit
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int 0, n, m;
+
+	for (n = 0; n <= 7; n++)
+	{
+		for (m = n + 1; m <= 8; m++)
+		{
+			for (o = m + 1; o <= 9; o++)
+			{
+				putchar(n + '0');
+				putchar(m + '0');
+				putchar(o + '0');
+			if (n != 7 || m != 8 || o != 9)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
