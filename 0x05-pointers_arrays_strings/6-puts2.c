8c19aa127a7ce0ccef1460ca52b67f1f162522da
Samuel Osayomi <Samuelosayomi@github.com>
2023-10-20 02:41:40 -0700

    6 print every other char

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  puts2 - Function that prints every other char in a string
+ *  @str: Required string
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		if (i % 2 == 0)
+		{
+			_putcahr(str[i]);
+		}
+	_putcahr('\n');
+}
