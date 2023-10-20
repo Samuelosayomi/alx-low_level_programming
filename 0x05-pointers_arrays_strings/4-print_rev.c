9596469ae58f63fa3d4e62691c0b26ba7dd941be
Samuel Osayomi <Samuelosayomi@github.com>
2023-10-20 02:24:06 -0700

    update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,16 +7,27 @@
 
 void print_rev(char *s)
 {
-	int count;
+	int i, j = _strlen(s);
 
-	while (*(s + count) != '\0')
+	for (i = j - 1; i >=0; i--)
 	{
-		count++;
-	}
-
-	while (count--)
-	{
-		_putchar(*(s + (count)));
+		_putchar(s[i]);
 	}
 	_putchar('\n');
+}
+
+/**
+ * _strlen - Function that prints lenght of a string
+ * @s: Required string
+ * Return: 0 (string lenght)
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int count =0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+	retutn (count);
 }
