8217a2cb4e2c429170a187d8e91d7682f988202e
Samuel Osayomi <Samuelosayomi@github.com>
2023-10-20 01:10:45 -0700

    1 dont swap horses

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - Function that swaps value of 2 integers
+ * @a: first integer
+ * @b: second integer
+ */
+
+void swap_int(int *a, int *b)
+{
+	int 1;
+
+	i = *a;
+	*a = *b;
+	*b = i;
+}
